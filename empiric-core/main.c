cee64fbb75b5d86cc951e9326a561e8da9cd4c12
Md. Rafiqul Islam Shafat <rishafat435@gmail.com>
2026-05-20 22:42:21 +0600

    Make a placeholder ai html to write the backend

--- empiric-core/main.c
+++ empiric-core/main.c
@@ -2,11 +2,19 @@
 #include <string.h>
 
 int main(void) {
+    //well initialising a big buffer
     char line[1024];
 
+    //looping until EOF which is essentialy null breaking the loop
     while (fgets(line, sizeof(line), stdin)) {
+        //strcspn gives the length of the string before \n/
+        //since it counts from the number represents the after charachters index from string or of the "\n"s index
+        //we replace \n with \0 to mark the end of the string
+        //essentially replace \n with \0
         line[strcspn(line, "\n")] = '\0';
+        //i mean it prints {"status": "ok", "echo": "string"} n goes to next line;
         printf("{\"status\": \"ok\", \"echo\": \"%s\"}\n", line);
+        //it forces output each time and clears the buffer for taking input
         fflush(stdout);
     }
 
